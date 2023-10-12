3541385bd2dca66e7155d9d7ad88bd17e4ccae36
Simon Larsen <sjl@esoft.com>
2023-10-12 14:50:02 +0200

    Bump version to 0.1.3

--- stort/__init__.py
+++ stort/__init__.py
@@ -1,7 +1,7 @@
 """
 .. include:: ../README.md
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .pipelines.supervised import train_supervised  # noqa: F401
 from .config import read_json_config  # noqa: F401
