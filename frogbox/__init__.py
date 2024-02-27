4fecc3b21cff2b9c789613bfb071be4d6e57d49e
Simon Larsen <sjl@esoft.com>
2024-02-27 14:54:49 +0100

    Bump version to 0.3.0 for name change

--- frogbox/__init__.py
+++ frogbox/__init__.py
@@ -1,7 +1,7 @@
 """
 .. include:: ./intro.md
 """
-__version__ = "0.2.4b"
+__version__ = "0.3.0"
 
 from .pipelines.supervised import SupervisedPipeline  # noqa: F401
 from .config import read_json_config  # noqa: F401
