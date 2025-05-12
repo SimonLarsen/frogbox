18485c0436a4ed7fd777773852bde99c698a7223
Simon J. Larsen <sjl@esoft.com>
2025-05-12 13:12:40 +0000

    Add test to ensure import_name and dist-name versions correspond

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,6 +1,12 @@
 from packaging.version import Version
+import importlib
 
 
 def test_import():
     import frogbox
     Version(frogbox.__version__)
+
+
+def test_import_dist_versions():
+    import frogbox
+    assert frogbox.__version__ == importlib.metadata.version("frogbox")
